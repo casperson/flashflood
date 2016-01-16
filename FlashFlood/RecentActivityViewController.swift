668cb4bf05275903a40d88af36b9f6079d4a32c2
Braden Casperson <braden.casperson@canopytax.com>
2016-01-16 13:30:28 -0600

    Added lots of view controllers, organized into files MVC, and updated main storyboard.

---
+++ FlashFlood/RecentActivityViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RecentActivityViewController.swift
+//  FlashFlood
+//
+//  Created by Braden Casperson on 1/15/16.
+//  Copyright © 2016 Braden Casperson. All rights reserved.
+//
+
+import UIKit
+
+class RecentActivityViewController : UITableViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Segues
+    
+    
+    // MARK: - Table view data source
+    
+    
+}
