148be6ed3151d178875af6f96fd04c570a466d4b
Braden Casperson <braden.casperson@canopytax.com>
2016-01-25 16:32:52 -0700

    Added AWS Mobile SDK to project and setup CodeCommit repo.

---
+++ FlashFlood/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  FlashFlood
+//
+//  Created by Braden Casperson on 1/25/16.
+//  Copyright © 2016 Braden Casperson. All rights reserved.
+//
+
+import Foundation
+
+let CognitoRegionType = AWSRegionType.USEast1
+let DefaultServiceRegionType = AWSRegionType.USEast1
+let CognitoIdentityPoolId = "YourCognitoIdentityPoolId"
+let AWSSampleDynamoDBTableName = "DynamoDB-OM-SwiftSample"
---
+++ FlashFlood/ObjectiveC_Bridging_Header.h
@@ -0,0 +1,30 @@
+//
+//  ObjectiveC_Bridging_Header.h
+//  FlashFlood
+//
+//  Created by Braden Casperson on 1/25/16.
+//  Copyright © 2016 Braden Casperson. All rights reserved.
+//
+
+#import <AWSAutoScaling/AWSAutoScaling.h>
+#import <AWSCloudWatch/AWSCloudWatch.h>
+#import <AWSCognito/AWSCognito.h>
+#import <AWSCore/AWSCore.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSEC2/AWSEC2.h>
+#import <AWSElasticLoadBalancing/AWSElasticLoadBalancing.h>
+#import <AWSKinesis/AWSKinesis.h>
+#import <AWSLambda/AWSLambda.h>
+#import <AWSMachineLearning/AWSMachineLearning.h>
+#import <AWSMobileAnalytics/AWSMobileAnalytics.h>
+#import <AWSS3/AWSS3.h>
+#import <AWSSES/AWSSES.h>
+#import <AWSSQS/AWSSQS.h>
+#import <AWSSNS/AWSSNS.h>
+#import <AWSSimpleDB/AWSSimpleDB.h>
+
+#ifndef ObjectiveC_Bridging_Header_h
+#define ObjectiveC_Bridging_Header_h
+
+
+#endif /* ObjectiveC_Bridging_Header_h */
